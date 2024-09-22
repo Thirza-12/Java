4cf5416f31c205bc2b5d8c5e2a5a3f3361c524dd
Thirza-12 <thirzabehal@gmail.com>
2024-09-22 20:10:11 +0530

    Datatypes in java

--- src/Datatypes_In_Java/Datatypes.java
+++ src/Datatypes_In_Java/Datatypes.java
@@ -2,7 +2,34 @@ package Datatypes_In_Java;
 
 public class Datatypes {
     public static void main(String[] args) {
+
+        // Primitive Datatypes
+        int num1=9;
         float marks = 70.5f;
+        double d=80.90;
+        char c='K';
+        byte b=127;
+        short sh=558;
+        long l=57564766554434345L;
+        boolean bool=true; // you can't write 1 or 0 here as it will cause error that int cant be converted to boolean
+
+        int zeroes= 10_0787_000; // You can put underscores also to make your code easy looking
+        System.out.println(zeroes);
+
+        double ex=12e5; // this means 12*10^5
+        System.out.println(ex);
+
+        char ch='a';
+        ch++; // can be done
+        // ch=ch+1 can not be done as it will say lossy conversion from int to char
+        System.out.println(ch); // this will print b;
+        // Numbers in binary and hexadecimal format
+        int binary=0b101; // put 0b in first-place to write binary numbers
+        System.out.println(binary);
+
+        int hex=0x7E; // put 0x in first-place to write hexadecimal numbers
+        System.out.println(hex);
+
 
     }
 }
