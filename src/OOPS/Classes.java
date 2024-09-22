f0037e4ee979668eb6b53653104677d9204cac7e
Thirza-12 <thirzabehal@gmail.com>
2024-09-22 23:09:15 +0530

    Classes

---
+++ src/OOPS/Classes.java
@@ -0,0 +1,21 @@
+package OOPS;
+class Calculate {
+    int num1;
+    int num2;
+
+    public int add(int n1,int n2)
+    {
+        this.num1=n1;
+        this.num2=n2;
+
+        return num1+num2;
+    }
+}
+public class Classes {
+    public static void main(String[] args) {
+
+        Calculate calc=new Calculate();
+        int result=calc.add(3,4);
+        System.out.println(result);
+    }
+}
